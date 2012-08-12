14dbec9ca481bbb45d3312d796a2b26f75600e01
Etern8ty <jay@etern8ty.com>
2012-08-12 10:29:39 -0700

    Basic outline

--- gen/com/etern8ty/tasker/R.java
+++ gen/com/etern8ty/tasker/R.java
@@ -15,8 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int editText1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int holder=0x7f070003;
+        public static final int inputBox=0x7f070001;
+        public static final int menu_settings=0x7f070004;
+        public static final int scrollBox=0x7f070002;
         public static final int submit=0x7f070000;
     }
     public static final class layout {
--- src/com/etern8ty/tasker/Tasker.java
+++ src/com/etern8ty/tasker/Tasker.java
@@ -1,11 +1,24 @@
 package com.etern8ty.tasker;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Tasker extends Activity {
 
+	private ArrayList<String> ToDoList;
+	
+	public Tasker(){
+		super();
+		ToDoList = new ArrayList<String>();
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +30,26 @@ public class Tasker extends Activity {
         getMenuInflater().inflate(R.menu.activity_tasker, menu);
         return true;
     }
+    
+    public void submitTask(View viewer){
+    	addRow();
+    	printList();
+    }
+    
+    private void addRow(){
+    	EditText inputBox = (EditText) findViewById(R.id.inputBox);
+    	ToDoList.add(inputBox.getText().toString());
+    	inputBox.clearComposingText();
+    }
+    
+    private void printList(){
+    	LinearLayout ToDoHolder = (LinearLayout) findViewById(R.id.holder);
+    	for(int i=0; i<ToDoList.size(); i++){
+    		TextView ToDoItem = new TextView(this);
+    		ToDoItem.setId(i);
+    		ToDoItem.setText(ToDoList.get(i));
+    		ToDoHolder.addView(ToDoItem);
+    	}
+    }
+    
 }
